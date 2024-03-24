0898e070e9b6713964bad21ab7979b849f7c7434
MaruyamTaro <21300@wc.school.nz>
2024-03-25 12:25:03 +1300

    Added the choice of going to scavenge or not and started on the upgrade funtion

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ food = 10
 
 # Functions
 
-def menu():#The Menu Function that gets used in the beginning
+def menu():  # The Menu Function that gets used in the beginning
     global difficulty
-    print("This is a Base building game set in a zombie apocalypse!\nTo win you have to survive 100 days!\nUpgrade your gear to scavenge more materials and protect your base!")
+    print(
+        "This is a Base building game set in a zombie apocalypse!\nTo win you have to survive 100 days!\nUpgrade your gear to scavenge more materials and protect your base!")
     difficulty = input("What difficulty do you want to play! (1 for Easy) (2 for Hard) (3 for unfair)")
 
     player_input = input("If you want to play, press 1. If you want to quit press 2")
@@ -28,33 +29,50 @@ def menu():#The Menu Function that gets used in the beginning
 
 def game():
     print("you start off with " + str(people) + " people " + str(wood) + " wood." + str(scrap) + "scrap and " + str(
-        people) + " people.")
+        food) + " food.")
     while food >= 0 or people >= 0 or day_count >= 100:
-        pass
-
+        day()
     if food >= 0:
-        print("you starved")
+        print("Everyone starved. You lose")
+
     elif people >= 0:
-        print("you have no more people left")
+        print("you have no more people left. You lose")
+
+    if food >= 0 and people >= 0 and day_count >= 100:
+        print("You win! you survived for 100 days!")
+
+def upgrade():   #function for player to upgrade gear and Base with materials.
+    pass
 
 
 
-def generator():
+
+
+def generator():  #player gaining materials after scavenging
     global wood, scrap, people, food
     wood_found = 0
     scrap_found = 0
     people_found = 0
 
-    print("With the tools you have today you scavenged " + wood_found + "wood and " + scrap_found + "scrap and you found " + people_found)
-    print("Do you want to upgrade your tools for the next day? ")
+    print(
+        "With the tools you have today you scavenged " + wood_found + "wood and " + scrap_found + "scrap and you found " + people_found)
+
 
 def day():
     global day_count
     day_count += 1
     print("This is day ", int(day_count))
-    generator()
+    try:
+        playerInput = int(input("Do you want your team to go scavenging for materials? Some might die. 1 for Yes 2 for no"))
+        if playerInput == 1:
+            generator()
+
+        elif playerInput == 2:
+            upgrade()
+
+    except ValueError:
+        print("Invalid input Try again")
 
 
 # Main
 menu()
-#generator()
