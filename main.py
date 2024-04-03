36da911664b0e95f4f95c84b6ac53ff3eb6b4985
21300 <21300@wc.school.nz>
2024-04-03 14:17:07 +1300

    Worked on generator day count. the counting day section is broken needs fixing
    worked on how each day will work

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ food = 10
 current_gear = 0
 
 
-
+ITEMS = []
+INVENTORY = []
 
 
 #lists and dictionary
 #This will be values that will be used when upgrading
 UPGRADES = [
     {"BASE_UPGRADES": [{"wall_1": 0.1, "wall_2": 0.3, "wall_1": 0.4, "wall_1": 0.5}]}
-    , {"GEAR_UPGRADES": [{"scav_1": 0.1, "scav_2": 0.3, "scav_3": 0.4, "scav_4": 0.5}]}]
+    , {"GEAR_UPGRADES": [{"Level 1 gear": 1.2, "Level 2 gear": 1.4, "Level 3 gear": 1.8, "Level 4 gear": 2}]}]
 #these are the values that will change depending on the difficulty
 EASY = [
         {"CHANCES": [{"zombie": 0.1, "scav_death": 0.1, "food_rate": 0.2, "wood_rate": 200, "scrap_rate": 200, "people_rate": 2, "people_death": 2}]}
@@ -42,7 +43,7 @@ def menu():
     global difficulty, EASY, HARD, UNFAIR
     print(
         "This is a Base building game set in a zombie apocalypse!\nTo win you have to survive 100 days!\nUpgrade your gear to scavenge more materials and protect your base!")
-    playerInput = input("What difficulty do you want to play! (1 for Easy) (2 for Hard) (3 for unfair)")
+    playerInput = input("What difficulty do you want to play! (1 for Easy) (2 for Hard) (3 for unfair)\n")
 
     if playerInput == "1":
         difficulty = EASY
@@ -53,8 +54,9 @@ def menu():
     if playerInput == "3":
         difficulty = UNFAIR
     try:
-        player_input = input("If you want to play, press 1. If you want to quit press 2")
+        player_input = input("If you want to play, press 1. If you want to quit press 2\n")
         if player_input == "1":
+            os.system('cls')
             game()
         if player_input == "2":
             quit()
@@ -65,7 +67,9 @@ def menu():
 def game():
     print("You wander around with your team and finally found a place you can stay and make camp\nYou Can go scavenging for people, scrap, and wood. Per day your food will go down depending on the number of people\n"
           "you will need to upgrade your gear for more materials scavenging and upgrade your base to defend from raids.")
-    print("you start off with " + str(people) + " people " + str(wood) + " wood" + str(scrap) + " scrap and " + str(food) + " food.")
+    input("Press Enter to continue...")
+    os.system('cls')
+    print("you start off with " + str(people) + " people, " + str(wood) + " wood, " + str(scrap) + " scrap, and " + str(food) + " food.")
     day()
 
 
@@ -90,42 +94,51 @@ def generator():
     people = people-people_dead
     print("you scavenged for a few hours in a close city with leftover loot")
     if people_dead >= 0:
-        print("Unfortunately ", people_dead, "many people died")
+        print("Unfortunately ", people_dead, " people died")
 
-    print("With the tools you have today, you scavenged " + str(wood_found) + "wood and " + str(scrap_found) + "scrap and you found " + str(people_found))
+    print("With the tools you have today, you scavenged " + str(wood_found) + " wood and " + str(scrap_found) + " scrap and you found " + str(people_found))
+    print("After a long day of scavenging you go home to rest and prepare for the next day.")
+    input("Press Enter to continue...")
+    day()
 
 
 def day():
-    global day_count
+    global day_count,food
     #calculates how much food per day
     food_per_day = difficulty[0]["CHANCES"][0]["food_rate"]
     food -= people * food_per_day
     day_count += 1
     print("This is day ", int(day_count))
-    print("you Have " + str(people) + " people " + str(wood) + " wood" + str(scrap) + " scrap and " + str(food) + " food.")
-    print("Your team ate", str(food), "today")
+    print("Your team ate", str(food), "kilos of food today")
+    print("you Have " + str(people) + " people, " + str(wood) + " wood, " + str(scrap) + " scrap, and " + str(food) + " food.")
     try:
-        playerInput = int(input("Do you want your team to go scavenging for materials? Some might die. 1 for scavenging  2 for upgrades"))
+        playerInput = int(input("Do you want your team to go scavenging for materials? Some might die. 1 for scavenging  2 for upgrades\n"))
         if playerInput == 1:
+            os.system('cls')
             generator()
 
         elif playerInput == 2:
+            os.system('cls')
             upgrade()
 
     except ValueError:
         print("Invalid input Try again")
 
 
+
     while food >= 0 or people >= 0 or day_count >= 100:
         day()
     if food <= 0:
         print("Everyone starved. You lose")
+        input("Press Enter to continue...")
 
     elif people <= 0:
         print("you have no more people left. You lose")
+        input("Press Enter to continue...")
 
     if food >= 0 and people >= 0 and day_count >= 100:
         print("You win! you survived for 100 days!")
+        input("Press Enter to continue...")
 
 
 
